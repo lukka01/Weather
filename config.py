b73adc181bccfbe0d5f5f03d2b38d0bc45498430
My Computer <khurtsidzeluka0@gmail.com>
2026-04-09 23:59:37 +0400

    your message

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_size": 10,      #ერთდროულად მაქსიმუმ 10 კავშირი
-        "max_overflow": 20,   # პიკზე კიდევ 20 დამატებითი
+        "pool_size": 10,      # ერთდროულად მაქსიმუმ 10 კავშირი
+        "max_overflow": 20,
         "pool_recycle": 1800, # 30 წუთში განხალდბა კავშირი
     }
